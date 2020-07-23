b16892e1e7a26e0350d5035efd129159982cd5b4
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 20:12:15 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -4,12 +4,4 @@ module MetaDancing
     "This class produces objects that love to dance."
   end
   
-  class Dancer
-    extend MetaDancing
-  end
-  
-  class Kid 
-    extend MetaDancing
-  end
-  
 end
