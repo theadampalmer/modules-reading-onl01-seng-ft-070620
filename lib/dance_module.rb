b2b223ea695e47489149870a1664d0ce2e859ca4
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 20:01:36 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -12,7 +12,8 @@ module Dance
   end
   
   def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all."
+    "Thank you, thank you.
+      It was a pleasure to dance for you all."
   end
   
 end
